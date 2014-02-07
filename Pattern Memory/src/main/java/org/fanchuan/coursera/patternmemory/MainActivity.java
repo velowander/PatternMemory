1479aba6722581ba87bdb142a408a1b947ad46cf
David Monk <velowander@users.noreply.github.com>
2014-02-07 17:00:01 +0800

    Basic game works, it keeps track of score (1 point per correctly clicked tile) and round number. Has basic help dialog. Still no sound.

--- Pattern Memory/src/main/java/org/fanchuan/coursera/patternmemory/MainActivity.java
+++ Pattern Memory/src/main/java/org/fanchuan/coursera/patternmemory/MainActivity.java
@@ -1,6 +1,7 @@
 package org.fanchuan.coursera.patternmemory;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -26,11 +27,9 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void onClickBeginGame(View vw) {
-        TextView tvwScore = (TextView) findViewById(R.id.tvwScore);
-        tvwScore.setText(Integer.toString(0));
         simon = initializeSimon();
         SimonListener listener = new SimonListener(simon);
-        simon.noteRandomAdd(4);
+        simon.noteRandomAdd();
         simon.play();
     }
 
@@ -51,16 +50,24 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.help_action) {
+            showHelp();
+            return true;
+        } else {
+            //If I make a settings dialog, show it here.
             return true;
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    public void showHelp() {
+        Dialog help = new Dialog(this);
+        help.setContentView(R.layout.dialog_help);
+        help.show();
     }
 
     private class Simon {
@@ -156,17 +163,21 @@ public class MainActivity extends ActionBarActivity {
             for (View deviceButton : simonButtons) {
                 deviceButton.setOnClickListener(this);
             }
+            TextView tvwScore = (TextView) findViewById(R.id.tvwScore);
+            TextView tvwRound = (TextView) findViewById(R.id.tvwRound);
+            tvwScore.setText(Integer.toString(0));
+            tvwRound.setText(Integer.toString(0));
         }
 
         public void onClick(View vw) {
             Activity activity = (Activity) vw.getContext();
-            TextView tvwScore = (TextView) activity.findViewById(R.id.tvwScore);
-            if (tvwScore == null)
-                Log.w(TAG, "tvwScore is null");
 
             if (simon.verifyButtonPress(vw)) {
                 //This button press matches the current one in the Iterator, get a point whether it is the last or not
                 try {
+                    TextView tvwScore = (TextView) activity.findViewById(R.id.tvwScore);
+                    if (tvwScore == null)
+                        Log.w(TAG, "tvwScore is null");
                     int score = Integer.parseInt(tvwScore.getText().toString());
                     score++;
                     tvwScore.setText(Integer.toString(score));
@@ -175,9 +186,18 @@ public class MainActivity extends ActionBarActivity {
                 }
                 if (!mySimon.hasNext()) {
                     //No more buttons to press, at the end of the melody!! Next round.
-
+                    try {
+                        TextView tvwRound = (TextView) activity.findViewById(R.id.tvwRound);
+                        int round = Integer.parseInt(tvwRound.getText().toString());
+                        round++;
+                        tvwRound.setText(Integer.toString(round));
+                    } catch (Exception e) {
+                        Log.e(TAG, "Unable to set round", e);
+                    }
                     String messageRoundWin = activity.getResources().getString(R.string.message_round_win);
                     Toast.makeText(activity, messageRoundWin, Toast.LENGTH_SHORT).show();
+                    mySimon.noteRandomAdd();
+                    mySimon.play();
                 }
             } else {
                 String messageLose = activity.getResources().getString(R.string.message_lose);
