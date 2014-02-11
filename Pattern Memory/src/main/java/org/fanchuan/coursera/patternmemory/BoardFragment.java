86b1666fc6eb6cf274fc72f1401586125e9996b5
David Monk <velowander@users.noreply.github.com>
2014-02-11 14:50:31 +0800

    Game works, keeps track of round and score. There is no sound or high score. Internal refactor: Game board is now a separate fragment in the static xml main activity (so there are 2 total fragments)

--- Pattern Memory/src/main/java/org/fanchuan/coursera/patternmemory/BoardFragment.java
+++ Pattern Memory/src/main/java/org/fanchuan/coursera/patternmemory/BoardFragment.java
@@ -6,11 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ScoreBarFragment extends Fragment {
+public class BoardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score_bar, container, false);
+        return inflater.inflate(R.layout.fragment_board, container, false);
     }
 }
